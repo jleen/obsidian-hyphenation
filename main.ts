a569390c98fd94cc8e1b21b81de72df4b91a498c
John Leen <jleen@saturnvalley.org>
2022-08-09 20:23:37 -0400

    Documentation and identification

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
 import { Plugin } from "obsidian"
 import { hyphenateHTMLSync } from "hyphen/en"
 
-export default class ExamplePlugin extends Plugin {
-	async onload() {
-		this.registerMarkdownPostProcessor((element, context) => {
-			const chunks = element.querySelectorAll("p")
+export default class HyphenationPlugin extends Plugin {
+  async onload() {
+    this.registerMarkdownPostProcessor((element, context) => {
+      const chunks = element.querySelectorAll("p")
 
-			chunks.forEach((chunk) => {
-				chunk.innerHTML = hyphenateHTMLSync(chunk.innerHTML)
-			})
-		})
-	}
+      chunks.forEach(chunk => {
+        chunk.innerHTML = hyphenateHTMLSync(chunk.innerHTML)
+      })
+    })
+  }
 }
